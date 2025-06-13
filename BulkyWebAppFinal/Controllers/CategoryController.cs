b858c8769d75b04d22bea739f2ac794fc986cf34
Tamimkhan7 <mdtamimkhan842@gmail.com>
2025-06-14 00:34:27 +0600

    added create and update categorycontroller and index of category

--- BulkyWebAppFinal/Controllers/CategoryController.cs
+++ BulkyWebAppFinal/Controllers/CategoryController.cs
@@ -1,10 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyWebAppFinal.Data;
+using BulkyWebAppFinal.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWebAppFinal.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+         public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
         {
             return View();
         }
