206322455a845ae9d7baae1b39ade17653c160db
Tamimkhan7 <mdtamimkhan842@gmail.com>
2025-06-13 21:48:57 +0600

    update applicationbdcontext and Added CategoryController and seedCategoryTable

--- BulkyWebAppFinal/Migrations/20250613154656_SeedCategoryTable.Designer.cs
+++ BulkyWebAppFinal/Migrations/20250613154656_SeedCategoryTable.Designer.cs
@@ -3,6 +3,7 @@ using BulkyWebAppFinal.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BulkyWebAppFinal.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250613154656_SeedCategoryTable")]
+    partial class SeedCategoryTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
