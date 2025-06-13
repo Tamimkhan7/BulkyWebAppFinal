46e4eb9380d2a3dd8cbeeeaeab9e1e1bae8b9c55
Tamimkhan7 <mdtamimkhan842@gmail.com>
2025-06-13 21:08:05 +0600

    modified bulkywebfinal and program file

---
+++ BulkyWebAppFinal/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BulkyWebAppFinal.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWebAppFinal.Data
+{
+    public class ApplicationDbContext: DbContext  
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>option) : base(option) 
+        { 
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
